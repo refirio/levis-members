3247c8259cb8c7a9cfd5bc66b3a43a48a791286a
refirio <refirio@gmail.com>
2016-02-07 21:46:45 +0900

    コードフォーマットの調整。

--- libs/plugins/validator.php
+++ libs/plugins/validator.php
@@ -14,11 +14,11 @@
  */
 function validator_required($data)
 {
-	if ($data === '') {
-		return false;
-	} else {
-		return true;
-	}
+    if ($data === '') {
+        return false;
+    } else {
+        return true;
+    }
 }
 
 /**
@@ -30,11 +30,11 @@ function validator_required($data)
  */
 function validator_min_length($data, $min)
 {
-	if (mb_strlen($data, MAIN_INTERNAL_ENCODING) < $min) {
-		return false;
-	} else {
-		return true;
-	}
+    if (mb_strlen($data, MAIN_INTERNAL_ENCODING) < $min) {
+        return false;
+    } else {
+        return true;
+    }
 }
 
 /**
@@ -46,11 +46,11 @@ function validator_min_length($data, $min)
  */
 function validator_max_length($data, $max)
 {
-	if (mb_strlen($data, MAIN_INTERNAL_ENCODING) > $max) {
-		return false;
-	} else {
-		return true;
-	}
+    if (mb_strlen($data, MAIN_INTERNAL_ENCODING) > $max) {
+        return false;
+    } else {
+        return true;
+    }
 }
 
 /**
@@ -63,11 +63,11 @@ function validator_max_length($data, $max)
  */
 function validator_between($data, $min, $max)
 {
-	if (mb_strlen($data, MAIN_INTERNAL_ENCODING) < $min || mb_strlen($data, MAIN_INTERNAL_ENCODING) > $max) {
-		return false;
-	} else {
-		return true;
-	}
+    if (mb_strlen($data, MAIN_INTERNAL_ENCODING) < $min || mb_strlen($data, MAIN_INTERNAL_ENCODING) > $max) {
+        return false;
+    } else {
+        return true;
+    }
 }
 
 /**
@@ -78,11 +78,11 @@ function validator_between($data, $min, $max)
  */
 function validator_alpha($data)
 {
-	if (!preg_match('/^[_a-zA-Z]+$/', $data)) {
-		return false;
-	} else {
-		return true;
-	}
+    if (!preg_match('/^[_a-zA-Z]+$/', $data)) {
+        return false;
+    } else {
+        return true;
+    }
 }
 
 /**
@@ -93,11 +93,11 @@ function validator_alpha($data)
  */
 function validator_numeric($data)
 {
-	if (!preg_match('/^\d+$/', $data)) {
-		return false;
-	} else {
-		return true;
-	}
+    if (!preg_match('/^\d+$/', $data)) {
+        return false;
+    } else {
+        return true;
+    }
 }
 
 /**
@@ -108,11 +108,11 @@ function validator_numeric($data)
  */
 function validator_decimal($data)
 {
-	if (!is_numeric($data)) {
-		return false;
-	} else {
-		return true;
-	}
+    if (!is_numeric($data)) {
+        return false;
+    } else {
+        return true;
+    }
 }
 
 /**
@@ -123,11 +123,11 @@ function validator_decimal($data)
  */
 function validator_alpha_numeric($data)
 {
-	if (!preg_match('/^\w+$/', $data)) {
-		return false;
-	} else {
-		return true;
-	}
+    if (!preg_match('/^\w+$/', $data)) {
+        return false;
+    } else {
+        return true;
+    }
 }
 
 /**
@@ -138,11 +138,11 @@ function validator_alpha_numeric($data)
  */
 function validator_alpha_dash($data)
 {
-	if (!preg_match('/^[\w\-]+$/', $data)) {
-		return false;
-	} else {
-		return true;
-	}
+    if (!preg_match('/^[\w\-]+$/', $data)) {
+        return false;
+    } else {
+        return true;
+    }
 }
 
 /**
@@ -154,11 +154,11 @@ function validator_alpha_dash($data)
  */
 function validator_equals($data, $value)
 {
-	if ($data !== $value) {
-		return false;
-	} else {
-		return true;
-	}
+    if ($data !== $value) {
+        return false;
+    } else {
+        return true;
+    }
 }
 
 /**
@@ -170,11 +170,11 @@ function validator_equals($data, $value)
  */
 function validator_min($data, $min)
 {
-	if ($data < $min) {
-		return false;
-	} else {
-		return true;
-	}
+    if ($data < $min) {
+        return false;
+    } else {
+        return true;
+    }
 }
 
 /**
@@ -186,11 +186,11 @@ function validator_min($data, $min)
  */
 function validator_max($data, $max)
 {
-	if ($data > $max) {
-		return false;
-	} else {
-		return true;
-	}
+    if ($data > $max) {
+        return false;
+    } else {
+        return true;
+    }
 }
 
 /**
@@ -203,11 +203,11 @@ function validator_max($data, $max)
  */
 function validator_range($data, $min, $max)
 {
-	if ($data < $min || $data > $max) {
-		return false;
-	} else {
-		return true;
-	}
+    if ($data < $min || $data > $max) {
+        return false;
+    } else {
+        return true;
+    }
 }
 
 /**
@@ -218,11 +218,11 @@ function validator_range($data, $min, $max)
  */
 function validator_blank($data)
 {
-	if (!preg_match('/^\s*$/', $data)) {
-		return false;
-	} else {
-		return true;
-	}
+    if (!preg_match('/^\s*$/', $data)) {
+        return false;
+    } else {
+        return true;
+    }
 }
 
 /**
@@ -233,11 +233,11 @@ function validator_blank($data)
  */
 function validator_boolean($data)
 {
-	if (!is_bool($data) && !preg_match('/^(0|1)$/', $data)) {
-		return false;
-	} else {
-		return true;
-	}
+    if (!is_bool($data) && !preg_match('/^(0|1)$/', $data)) {
+        return false;
+    } else {
+        return true;
+    }
 }
 
 /**
@@ -249,11 +249,11 @@ function validator_boolean($data)
  */
 function validator_regexp($data, $regexp)
 {
-	if (!preg_match('/' . $regexp . '/', $data)) {
-		return false;
-	} else {
-		return true;
-	}
+    if (!preg_match('/' . $regexp . '/', $data)) {
+        return false;
+    } else {
+        return true;
+    }
 }
 
 /**
@@ -264,13 +264,13 @@ function validator_regexp($data, $regexp)
  */
 function validator_date($data)
 {
-	if (!preg_match('/^(\d\d\d\d)\-(\d\d)\-(\d\d)$/', $data, $matches)) {
-		return false;
-	} elseif (!checkdate($matches[2], $matches[3], $matches[1])) {
-		return false;
-	} else {
-		return true;
-	}
+    if (!preg_match('/^(\d\d\d\d)\-(\d\d)\-(\d\d)$/', $data, $matches)) {
+        return false;
+    } elseif (!checkdate($matches[2], $matches[3], $matches[1])) {
+        return false;
+    } else {
+        return true;
+    }
 }
 
 /**
@@ -281,13 +281,13 @@ function validator_date($data)
  */
 function validator_time($data)
 {
-	if (!preg_match('/^[0-2][0-9]\:[0-5][0-9]\:[0-5][0-9]$/', $data)) {
-		return false;
-	} else {
-		return true;
-	}
+    if (!preg_match('/^[0-2][0-9]\:[0-5][0-9]\:[0-5][0-9]$/', $data)) {
+        return false;
+    } else {
+        return true;
+    }
 
-	return true;
+    return true;
 }
 
 /**
@@ -298,13 +298,13 @@ function validator_time($data)
  */
 function validator_datetime($data)
 {
-	list($date, $time) = explode(' ', $data);
+    list($date, $time) = explode(' ', $data);
 
-	if (!validator_date($date) || !validator_time($time)) {
-		return false;
-	} else {
-		return true;
-	}
+    if (!validator_date($date) || !validator_time($time)) {
+        return false;
+    } else {
+        return true;
+    }
 }
 
 /**
@@ -315,11 +315,11 @@ function validator_datetime($data)
  */
 function validator_email($data)
 {
-	if (!preg_match('/^[^@\s]+@[^@\s]+$/', $data) || !validator_max_length($data, 256)) {
-		return false;
-	} else {
-		return true;
-	}
+    if (!preg_match('/^[^@\s]+@[^@\s]+$/', $data) || !validator_max_length($data, 256)) {
+        return false;
+    } else {
+        return true;
+    }
 }
 
 /**
@@ -330,11 +330,11 @@ function validator_email($data)
  */
 function validator_url($data)
 {
-	if (!preg_match('/^https?\:\/\//', $data)) {
-		return false;
-	} else {
-		return true;
-	}
+    if (!preg_match('/^https?\:\/\//', $data)) {
+        return false;
+    } else {
+        return true;
+    }
 }
 
 /**
@@ -345,11 +345,11 @@ function validator_url($data)
  */
 function validator_hiragana($data)
 {
-	if (!preg_match('/^[ぁ-ん]+$/u', $data)) {
-		return false;
-	} else {
-		return true;
-	}
+    if (!preg_match('/^[ぁ-ん]+$/u', $data)) {
+        return false;
+    } else {
+        return true;
+    }
 }
 
 /**
@@ -360,9 +360,9 @@ function validator_hiragana($data)
  */
 function validator_katakana($data)
 {
-	if (!preg_match('/^[ァ-ヶー]+$/u', $data)) {
-		return false;
-	} else {
-		return true;
-	}
+    if (!preg_match('/^[ァ-ヶー]+$/u', $data)) {
+        return false;
+    } else {
+        return true;
+    }
 }
