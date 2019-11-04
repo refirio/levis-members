bcf71b97fb1badd038da40d64bff26ab61c324f6
refirio <refirio@gmail.com>
2019-11-04 23:10:01 +0900

    エラーのタイミングによっては正しく画面が表示されない不具合を修正

--- app/views/error.php
+++ app/views/error.php
@@ -1,4 +1,15 @@
+<?php if (isset($GLOBALS['config'])) : ?>
 <?php import('app/views/header.php') ?>
+<?php else : ?>
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="<?php t(MAIN_CHARSET) ?>">
+        <title>Error</title>
+    </head>
+    <body>
+        <h1>Error</h1>
+<?php endif ?>
 
         <h2>エラー</h2>
 
@@ -7,4 +18,9 @@
         </ul>
         <p><a href="<?php t(empty($_SERVER['HTTP_REFERER']) ? MAIN_FILE . '/' : $_SERVER['HTTP_REFERER']) ?>">戻る</a></p>
 
+<?php if (isset($GLOBALS['config'])) : ?>
 <?php import('app/views/footer.php') ?>
+<?php else : ?>
+    </body>
+</html>
+<?php endif ?>
