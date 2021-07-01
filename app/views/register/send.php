abf7ba0d81f0b813b18b6b0715c0ca99d775b399
refirio <refirio@gmail.com>
2021-07-02 00:19:55 +0900

    不要なファイルを削除

--- app/views/register/send.php
+++
@@ -1,8 +0,0 @@
-<?php import('app/views/header.php') ?>
-
-        <h2><?php h($_view['title']) ?></h2>
-
-        <p>登録ページのURLを送信しました。</p>
-        <p>暗証コードは <code><?php h($_SESSION['expect']['token_code']) ?></code> です。</p>
-
-<?php import('app/views/footer.php') ?>
