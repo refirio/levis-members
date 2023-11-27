8a1dc9e86246dce5c103f432183f047cea3798c2
refirio <refirio@gmail.com>
2023-11-27 23:02:05 +0900

    levis9に合わせて調整（不要なコードを削除）

--- app/controllers/home/index.php
+++ app/controllers/home/index.php
@@ -1,13 +1,6 @@
 <?php
 
 // 教室を取得
-/*
-$_view['classes'] = select_classes(array(
-    'order_by' => 'sort, id',
-));
-*/
 $_view['classes'] = model('select_classes', [
     'order_by' => 'sort, id',
-    //'order_by' => 'id DESC',
 ]);
-//model(null);
